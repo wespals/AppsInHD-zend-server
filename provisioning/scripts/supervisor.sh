129e681a86959421def3dfa836acd20e35f8443e
Wes Pals <wpals@harrisdata.com>
2016-06-03 16:17:31 -0500

    additional supervisor configuration

--- provisioning/scripts/supervisor.sh
+++ provisioning/scripts/supervisor.sh
@@ -12,9 +12,10 @@ if [ ! -f /usr/bin/supervisord ]
     echo "Installing Supervisor"
     apt-get install -y supervisor
     echo "Configuring Supervisor"
+    cp /vagrant/provisioning/config/supervisor/supervisord.conf /etc/supervisor/supervisord.conf
     cp /vagrant/provisioning/config/supervisor/WebUI.conf /etc/supervisor/conf.d/WebUI.conf
     sed -i -e "s/ADMIN_USER_VAL/$ADMIN_USER/; s/ADMIN_PASS_VAL/$ADMIN_PASS/" /etc/supervisor/conf.d/WebUI.conf
-    #cp /vagrant/provisioning/config/apache2/AppsInHD.conf /etc/supervisor/conf.d/AppsInHD.conf
+    #cp /vagrant/provisioning/config/supervisor/AppsInHD.conf /etc/supervisor/conf.d/AppsInHD.conf
     #sed -i -e "s/HD_DB_VAL/$HD_DB/; s/HD_ENV_VAL/$HD_ENV/" /etc/supervisor/conf.d/AppsInHD.conf
     service supervisor restart
 fi
