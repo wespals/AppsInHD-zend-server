841062ad941240fcf1d127cfb4cb0ab251fa631c
Wes Pals <wpals@harrisdata.com>
2016-06-09 09:17:08 -0500

    set default server time to America/Chicago

--- provisioning/scripts/base.sh
+++ provisioning/scripts/base.sh
@@ -5,6 +5,9 @@ DOMAIN=$2
 
 echo "Provisioning base virtual machine..."
 
+echo "America/Chicago" > /etc/timezone
+dpkg-reconfigure -f noninteractive tzdata
+
 apt-get update
 
 grep -q -F "${IP} ${DOMAIN}" /etc/hosts || echo "${IP} ${DOMAIN}" >> /etc/hosts
