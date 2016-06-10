9a384d843ac04588eb726d2986bec249b2d5d199
Wes Pals <wpals@harrisdata.com>
2016-06-10 15:02:06 -0500

    move PHP timezone setting to ZS bootstrap

--- provisioning/scripts/zend-server.sh
+++ provisioning/scripts/zend-server.sh
@@ -15,7 +15,6 @@ if [ ! -f /usr/local/zend/bin/zs-manage ]
     tar -zxvf ZendServer-${ZS_VERSION}-RepositoryInstaller-linux.tar.gz
     cd ZendServer-RepositoryInstaller-linux/
     ./install_zs.sh ${PHP_VERSION} --automatic
-    sed -i -e "s/date.timezone=.*$/date.timezone=America\/Chicago/g" /usr/local/zend/etc/php.ini
 fi
 
 echo "Zend Server ${ZS_VERSION} Installed"
@@ -26,6 +25,7 @@ if [ ! -f /home/vagrant/zs-bootstrapped ]
     # Bootstrap Zend server
     # See http://files.zend.com/help/Zend-Server/content/bootstrap-single-server.htm
     /usr/local/zend/bin/zs-manage bootstrap-single-server -p ${ZS_ADMIN_PASSWORD} -a True -r False -N ${ZS_WEBAPI_KEY_NAME} -K ${ZS_WEBAPI_KEY_SECRET}
+    sed -i -e "s/date.timezone=.*$/date.timezone=America\/Chicago/g" /usr/local/zend/etc/php.ini
     /usr/local/zend/bin/zendctl.sh restart
     touch /home/vagrant/zs-bootstrapped
 fi
